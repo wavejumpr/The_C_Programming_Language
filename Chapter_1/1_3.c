366e210e382432997e1cf3403ba1188d53048dcd
Peter <peterincorvaja@protonmail.com>
2023-01-27 08:51:07 +0100

    hello, world and temperature conversion

---
+++ Chapter_1/1_3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define LOWER	0		// lower limit
+#define UPPER	300		// upper limit
+#define STEP	20		// step size
+
+/* print Fahrenheit-Celsius table
+ * for fahr = 0, 20, ..., 300 */
+int main()
+{
+	float fahr, celsius;
+	
+	fahr = LOWER;
+	printf("Fahrenheit\tCelsius\n");
+	while (fahr <= UPPER)
+	{
+		celsius = 5 * (fahr-32) / 9;
+		printf("%8.1f F\t%5.1f C\n", fahr, celsius);
+		fahr += STEP;
+	}
+}
---
+++ Chapter_1/1_4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define LOWER	0		// lower limit
+#define UPPER	300		// upper limit
+#define STEP	20		// step size
+
+/* print Celsius-Fahrenheit table
+ * for fahr = 0, 20, ..., 300 */
+int main()
+{
+	float fahr, celsius;
+	
+	celsius = LOWER;
+	printf("Celsius\tFahrenheit\n");
+	while (celsius <= UPPER)
+	{
+		fahr = (celsius*9/5) + 32;
+		printf("%5.1f C\t%8.1f F\n", celsius, fahr);
+
+		celsius += STEP;
+	}
+}
